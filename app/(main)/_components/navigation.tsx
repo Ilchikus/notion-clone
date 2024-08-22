8fcd2955363e052e0d779d3ba43cd74846e0cd7d
Illia Korchahin <theilchikus@gmail.com>
2024-08-22 21:56:09 +0200

    navbar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,7 +6,7 @@ import { useSettings } from "@/hooks/use-settings";
 
 
 import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings, Plus, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
@@ -21,10 +21,12 @@ import {
   PopoverContent,
 } from "@/components/ui/popover"
 import { TrashBox } from "./trash-box";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
   const search = useSearch();
   const settings = useSettings();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create)
@@ -194,11 +196,18 @@ export const Navigation = () => {
           isMobile && "left-0 w-full",
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar
+            isCollapsed={isCollapsed}
+            onResetWidth={resetWidth}
+          />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
