a0cd38d32273181637bed6d5ce22b3c56ec1c21a
Illia Korchahin <theilchikus@gmail.com>
2024-08-25 22:29:51 +0200

    cover image advanced

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -5,6 +5,7 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface DocumentIdPageProps {
     params: {
@@ -22,7 +23,15 @@ const DocumentIdPage = ({
     if (document === undefined) {
         return (
             <div>
-                Loading...
+                <Cover.Skeleton />
+                <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+                    <div className="space-y-4 pl-8 pt-4">
+                        <Skeleton className="h-14 w-1/2"/>
+                        <Skeleton className="h-4 w-4/5"/>
+                        <Skeleton className="h-14 w-2/5"/>
+                        <Skeleton className="h-14 w-3/5"/>
+                    </div>
+                </div>
             </div>
         )
     }
